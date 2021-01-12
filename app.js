f59e209a0572410d60dde709300996b61e187534
mtrupiano <57082946+mtrupiano@users.noreply.github.com>
2021-01-11 17:45:52 -0800

    Add logic for generating html file

--- app.js
+++ app.js
@@ -9,16 +9,11 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
 const Employee = require("./lib/Employee");
 
-
-const newManager = new Manager();
-const newIntern = new Intern();
-const newEngineer = new Engineer("Mark");
-const newEmployee = new Employee("Mark");
-
-console.log(newEngineer.getName());
-console.log(newEngineer.getRole());
+const newManager = new Manager("Joe", 5, "joe@gmail.com", 20);
+const newIntern = new Intern("Mark", 22, "mark@gmail.com", "UW");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -26,12 +21,21 @@ console.log(newEngineer.getRole());
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+const htmlStr = render([newManager, newIntern]);
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
+if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+}
+
+fs.writeFile(outputPath, htmlStr, (err) =>  {
+    if (err) { console.log(err); }
+});
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
